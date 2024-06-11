69557cefd8c45a3347e198315068de4591d7719d
CyberianEV <eaviutskyi@gmail.com>
2024-06-11 17:23:31 +0200

    add HW Spring with front on AngJs

---
+++ src/main/java/org/spring/sping_js/entities/Product.java
@@ -0,0 +1,45 @@
+package org.spring.sping_js.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private Double price;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,5 @@
 CREATE TABLE IF NOT EXISTS products (id BIGSERIAL PRIMARY KEY, title VARCHAR(255), price NUMERIC(10, 2));
 
-INSERT INTO products (title, price) VALUES ('Bread', 1.95), ('Milk', 0.95), ('Cheese', 3.20);
+INSERT INTO products (title, price)
+    VALUES ('Bread', 1.95), ('Milk', 0.95), ('Cheese', 3.20), ('Eggs', 1.05), ('Tomatoes', 2.65), ('Cucumbers', 0.65),
+            ('Chicken', 4.95), ('Pasta', 0.95), ('Cake', 4.60);
